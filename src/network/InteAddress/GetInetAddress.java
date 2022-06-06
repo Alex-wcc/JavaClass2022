cfaf56527d5aeb646f93e6ecbd434a02e1f0e3b2
ccWang <1508317304@qq.com>
2022-06-06 09:31:35 +0800

    期末补充

--- src/network/InteAddress/GetInetAddress.java
+++ src/network/InteAddress/GetInetAddress.java
@@ -2,7 +2,7 @@ package network.InteAddress;
 import java.net.*;
 public class GetInetAddress {
     public static void main(String args[]) throws Exception {
-        InetAddress inetAddress = InetAddress.getByName("www.baidu.com");
+        InetAddress inetAddress = InetAddress.getByName("www.google.com");
         System.out.println("主机名/IP: \t"+inetAddress);
         System.out.println("主机名: \t" +inetAddress.getHostName());
         System.out.println("IP: \t"+inetAddress.getHostAddress());
